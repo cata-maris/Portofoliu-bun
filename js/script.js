88204e627f6004ea8f80d5ec53aaf3e5f7dceae3
cata-maris <77330111+cata-maris@users.noreply.github.com>
2025-05-18 13:27:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -52,3 +52,66 @@ const cardItem = document.querySelector(".card-item");
 cardItem.addEventListener("click", function () {
   window.open("detalii.html", "_blank");
 });
+
+// pentru loader
+
+function showPage() {
+  document.querySelector(".loader-box").style.height = "0";
+  document.querySelector("#loader").style.display = "none";
+}
+
+function loaderTimeout() {
+  setTimeout(showPage, 2000);
+}
+
+window.addEventListener("load", loaderTimeout);
+
+console.log("Scriptul a fost încărcat!");
+
+// effect de scroll
+
+window.addEventListener("scroll", () => {
+  const section = document.getElementById("experience");
+  const indicator = document.querySelector(".scroll-bar");
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const scrollY = window.scrollY;
+
+  // Dacă suntem în interiorul secțiunii
+  if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+    const scrolledInSection = scrollY - sectionTop;
+
+    const scrollProgress = (scrolledInSection / sectionHeight) * 100 + 20;
+    let scrollProgressFinal = 0;
+    if (scrollProgress <= 80) {
+      scrollProgressFinal = scrollProgress;
+    } else {
+      scrollProgressFinal = 100;
+    }
+    indicator.style.height = scrollProgressFinal + "%";
+  } else if (scrollY < sectionTop) {
+    // Dacă nu am ajuns încă la secțiune
+    indicator.style.height = "15%";
+  } else {
+    // Dacă am trecut de secțiune
+    indicator.style.height = "100%";
+  }
+});
+// Reveal on scroll
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.9;
+
+  revealElements.forEach((el) => {
+    const boxTop = el.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      el.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
